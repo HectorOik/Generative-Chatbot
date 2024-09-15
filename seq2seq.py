477885f6b2c8bcec475569ec1c41811c85561fb9
HectorOik <oikohector@outlook.com>
2024-09-15 20:29:38 +0100

    programmed encoder/decoder and trained seq2seq model

--- seq2seq.py
+++ seq2seq.py
@@ -1,4 +1,38 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequencesm
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, LSTM, Dense, Embedding
+
+from preprocessing import max_input_length, max_target_length, vocab_size, input_sequences, target_sequences
+
+# Hyperparameters
+embedding_dim = 256
+lstm_units = 512
+
+epochs = 20
+batch_size = 64
+
+# Encoder
+encoder_inputs = Input(shape=(max_input_length,))
+encoder_embedding = Embedding(vocab_size, embedding_dim)(encoder_inputs)
+encoder_lstm = LSTM(lstm_units, return_state=True)
+encoder_outputs, state_h, state_c = encoder_lstm(encoder_embedding)
+encoder_states = [state_h, state_c]
+
+# Decoder
+decoder_inputs = Input(shape=(max_target_length,))
+decoder_embedding = Embedding(vocab_size, embedding_dim)(decoder_inputs)
+decoder_lstm = LSTM(lstm_units, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_embedding, initial_state=encoder_states)
+decoder_dense = Dense(vocab_size, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+# Define the model
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Print model summary
+model.summary()
+
+# training the model
+model.fit([input_sequences, input_sequences], target_sequences, epochs=epochs, batch_size=batch_size)
