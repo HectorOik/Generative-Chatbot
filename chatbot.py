64e5b1ead897b8a84df9083db6cf578fce0fdd2e
HectorOik <oikohector@outlook.com>
2024-09-16 11:58:32 +0100

    fixed dimensionality issues with input data and expected input data

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,9 @@
+import numpy as np
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+from preprocessing import tokenizer, max_input_length, max_target_length
+from seq2seq import encoder_lstm, decoder_lstm
+
 class Chatbot:
 
     negative_responses = ("no", "nope", "nah", "naw", "not a chance", "sorry")
@@ -5,7 +11,7 @@ class Chatbot:
     exit_commands = ("quit", "pause", "exit", "bye", "goodbye", "later", "stop")
 
     def start_chat(self):
-        user_response = input("Hi! I am a chatbot trained on human dialogue. Would you like to chat with me? ")
+        user_response = input("Hi! I am a chatbot trained on human dialogue. Would you like to chat with me?\n")
 
         if user_response in self.negative_responses:
             print("Ok, have a great day!")
@@ -18,8 +24,40 @@ class Chatbot:
             reply = input(self.generate_response(reply))
 
     def generate_response(self, user_input):
-        return "Cool\n"
-    
+        # Tokenize and pad input
+        input_seq = tokenizer.texts_to_sequences([user_input])
+        input_seq = pad_sequences(input_seq, maxlen=max_input_length, padding='post')
+
+        # Encode Input
+        states_value = encoder_lstm.predict(input_seq)
+
+        # Generate first token
+        target_seq = np.zeros((1, max_target_length))
+        target_seq[0, 0] = tokenizer.word_index.get('<start>', 0)
+
+        stop_condition = False
+        decoded_sentence = ''
+        
+        while not stop_condition:
+            output_tokens, h, c = decoder_lstm.predict([target_seq] + states_value)
+
+            # Sample a token
+            sampled_token_index = np.argmax(output_tokens[0, -1, :])
+            sampled_word = tokenizer.index_word.get(sampled_token_index, '')
+
+            # Exit condition: either hit max length or find <end>
+            if sampled_word == '<end>' or len(decoded_sentence) > max_target_length:
+                stop_condition = True
+            else:
+                decoded_sentence += ' ' + sampled_word
+
+            # Update the target sequence and states
+            target_seq = np.zeros((1, max_target_length))
+            target_seq[0, 0] = sampled_token_index
+            states_value = [h, c]
+
+        return decoded_sentence.strip() + '\n'
+
     def make_exit(self, reply):
         for exit_command in self.exit_commands:
             if exit_command in reply:
--- seq2seq.py
+++ seq2seq.py
@@ -1,38 +1,47 @@
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, LSTM, Dense, Embedding
+from tensorflow.keras.layers import Input, LSTM, Dense
 
-from preprocessing import max_input_length, max_target_length, vocab_size, input_sequences, target_sequences
+from preprocessing import (
+    encoder_input_data, decoder_input_data, decoder_target_data,
+    num_encoder_tokens, num_decoder_tokens, max_encoder_seq_length,
+    max_decoder_seq_length
+    )
 
 # Hyperparameters
-embedding_dim = 256
-lstm_units = 512
-
-epochs = 20
+dimensionality = 256
 batch_size = 64
+epochs = 100
 
 # Encoder
-encoder_inputs = Input(shape=(max_input_length,))
-encoder_embedding = Embedding(vocab_size, embedding_dim)(encoder_inputs)
-encoder_lstm = LSTM(lstm_units, return_state=True)
-encoder_outputs, state_h, state_c = encoder_lstm(encoder_embedding)
-encoder_states = [state_h, state_c]
+encoder_inputs = Input(shape=(None, num_encoder_tokens))
+# print(f'Encoder Inputs: {encoder_inputs.shape}')
+encoder_lstm = LSTM(dimensionality, return_state=True)
+encoder_outputs, state_hidden, state_cell = encoder_lstm(encoder_inputs)
+encoder_states = [state_hidden, state_cell] # encoder stastes to pass into the decoder
 
 # Decoder
-decoder_inputs = Input(shape=(max_target_length,))
-decoder_embedding = Embedding(vocab_size, embedding_dim)(decoder_inputs)
-decoder_lstm = LSTM(lstm_units, return_sequences=True, return_state=True)
-decoder_outputs, _, _ = decoder_lstm(decoder_embedding, initial_state=encoder_states)
-decoder_dense = Dense(vocab_size, activation='softmax')
-decoder_outputs = decoder_dense(decoder_outputs)
+decoder_inputs = Input(shape=(None, num_decoder_tokens))
+# print(f'Decoder Inputs: {decoder_inputs.shape}')
+decoder_lstm = LSTM(dimensionality, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+# print(f'Decoder Outputs: {decoder_outputs.shape}')
+decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs) # final softmax output
 
 # Define the model
 model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 
 # Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Print model summary
-model.summary()
+# model.summary()
 
 # training the model
-model.fit([input_sequences, input_sequences], target_sequences, epochs=epochs, batch_size=batch_size)
+model.fit(
+    [encoder_input_data, decoder_input_data],
+    decoder_target_data,
+    epochs=epochs, 
+    batch_size=batch_size, 
+    validation_split=0.2
+    )
